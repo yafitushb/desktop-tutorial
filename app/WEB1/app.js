beead29255454650ba8903fbcb604e5adc8ac4b4
yafitushb <47990022+yafitushb@users.noreply.github.com>
2022-08-17 15:05:23 +0300

    Add title

---
+++ app/WEB1/app.js
@@ -0,0 +1,17 @@
+function sayHello(name) {
+    console.log('Hello' + name );
+}
+
+//sayHello('Yafit');
+console.log(window);
+
+setTimeout()
+clearTimeout();
+
+setInterval()
+clearInterval()
+
+window.console.log
+
+var message = '';
+globalThis.setTimeout
---
+++ app/todolist/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> My First Project</h1> 
+      <h4> My project</h4>
+    </div>
+ 
+  );
+}
+
+export default App;
+
+
+
